25157202aa077dd34ccd5bb443e365d6c1556a5f
josermca01 <josermca01@gmail.com>
2024-09-13 18:40:56 -0300

    frontend-NLW-orbit

--- src/components/empty-goals.tsx
+++ src/components/empty-goals.tsx
@@ -1,5 +1,5 @@
-import logo from '../assets/logo-in-orbit.svg'
-import letsStart from '../assets/lets-start-illustration.svg'
+import logo from '../assets/in-orbit-logo.svg'
+import letsStart from '../assets/rocket-launch-illustration.svg'
 
 import { DialogTrigger } from './ui/dialog'
 import { Button } from './ui/button'
